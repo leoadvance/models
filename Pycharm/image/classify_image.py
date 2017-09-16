07d1b8f955453f52ace6afef78a946dfc9483867
leoadvance <leo_advance@163.com>
2017-09-16 20:40:52 +0800

    修改测试图片

--- Pycharm/image/classify_image.py
+++ Pycharm/image/classify_image.py
@@ -190,7 +190,7 @@ def maybe_download_and_extract():
 def main(_):
   maybe_download_and_extract()
   image = (FLAGS.image_file if FLAGS.image_file else
-           os.path.join(FLAGS.model_dir, 'cropped_panda.jpg'))
+           os.path.join(FLAGS.model_dir, 'cropped_panda2.jpg'))
   run_inference_on_image(image)
 
 
