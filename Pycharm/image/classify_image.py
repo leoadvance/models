459e5bbe93f4ff08bc4b353b9271b38c28fe9435
leoadvance <leo_advance@163.com>
2017-09-16 20:33:08 +0800

    修改储存路径

--- Pycharm/image/classify_image.py
+++ Pycharm/image/classify_image.py
@@ -35,6 +35,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
 import argparse
 import os.path
 import re
@@ -194,34 +195,35 @@ def main(_):
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  # classify_image_graph_def.pb:
-  #   Binary representation of the GraphDef protocol buffer.
-  # imagenet_synset_to_human_label_map.txt:
-  #   Map from synset ID to a human readable string.
-  # imagenet_2012_challenge_label_map_proto.pbtxt:
-  #   Text representation of a protocol buffer mapping a label to synset ID.
-  parser.add_argument(
+    print(os.path.dirname(__file__))
+    parser = argparse.ArgumentParser()
+    # classify_image_graph_def.pb:
+    #   Binary representation of the GraphDef protocol buffer.
+    # imagenet_synset_to_human_label_map.txt:
+    #   Map from synset ID to a human readable string.
+    # imagenet_2012_challenge_label_map_proto.pbtxt:
+    #   Text representation of a protocol buffer mapping a label to synset ID.
+    parser.add_argument(
       '--model_dir',
       type=str,
-      default='/tmp/imagenet',
+      default=os.path.dirname(__file__) + '/tmp/imagenet',
       help="""\
       Path to classify_image_graph_def.pb,
       imagenet_synset_to_human_label_map.txt, and
       imagenet_2012_challenge_label_map_proto.pbtxt.\
       """
-  )
-  parser.add_argument(
+    )
+    parser.add_argument(
       '--image_file',
       type=str,
       default='',
       help='Absolute path to image file.'
-  )
-  parser.add_argument(
+    )
+    parser.add_argument(
       '--num_top_predictions',
       type=int,
       default=5,
       help='Display this many predictions.'
-  )
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    )
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
